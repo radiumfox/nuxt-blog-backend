2e679068e5cd2b5c257e4843b19991cfd8720e73
Radium Fox <eteone.longa@gmail.com>
2023-11-09 19:14:20 +0100

    specify node version

--- api/index.ts
+++ api/index.ts
@@ -4,19 +4,19 @@ const mongoose = require("mongoose");
 const { resolvers } = require("./resolvers.ts");
 const { typeDefs } = require("../models/typeDefs.ts");
 
-// const MONGO_URI = "mongodb+srv://eteonelonga:0YlwOIihlKCAvCgW@cluster0.0qpxzjp.mongodb.net/radium_fox";
-//
-// mongoose
-//     .connect(MONGO_URI, {
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true,
-//     })
-//     .then(() => {
-//         console.log(`Db Connected`);
-//     })
-//     .catch(err => {
-//         console.log(err.message);
-//     });
+const MONGO_URI = "mongodb+srv://eteonelonga:0YlwOIihlKCAvCgW@cluster0.0qpxzjp.mongodb.net/radium_fox";
+
+mongoose
+    .connect(MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log(`Db Connected`);
+    })
+    .catch(err => {
+        console.log(err.message);
+    });
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
